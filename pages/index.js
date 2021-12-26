ce6174013bd9a8163e870e8c0676b552a361564f
happyhamsternft <96703108+happyhamsternft@users.noreply.github.com>
2021-12-26 14:35:45 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ export default function Home() {
       <main>
         <Header title="Welcome Happy Hamster Club!" />
         <p className="description">
-    Welcome to Happy Hamster Club.These hamsters are carefully made by hand, so each hamster is unique.5% of the profit are donated to a store full of cute hamsters.<code>
-          COMING SOON WEBSITE!<code>
+    
+          COMING SOON WEBSITE!<code>Welcome to Happy Hamster Club.These hamsters are carefully made by hand, so each hamster is unique.5% of the profit are donated to a store full of cute hamsters.<code>
         </p>
       </main>
 
